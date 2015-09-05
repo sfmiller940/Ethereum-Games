d348d5fa689961af0747ae0e0aa4a5b95dfafe64
sfmiller940 <sfm15@columbia.edu>
2015-09-05 11:38:52 -0700

    Create contract_rainbow.sol

---
+++ metacoin/contract_rainbow.sol
@@ -0,0 +1,18 @@
+contract rainbowCoin {
+	mapping (address => mapping (uint => uint)) balances;
+	function rainbowCoin() {
+		balances[msg.sender][0] = 10000; ///red coin
+		balances[msg.sender][1] = 10000; ///orange coin
+		balances[msg.sender][2] = 10000; ///yellow coin
+		balances[msg.sender][3] = 10000; ///green coin
+		balances[msg.sender][4] = 10000; ///blue coin
+		balances[msg.sender][5] = 10000; ///indigo coin
+		balances[msg.sender][6] = 10000; ///violet coin
+	}
+	function sendCoin(address receiver, uint amount, uint coin) returns(bool successful) {
+		if (balances[msg.sender][coin] < amount) return false;
+		balances[msg.sender][coin] -= amount;
+		balances[receiver][coin] += amount;
+		return true;
+	}
+}
