119b9f3353bdc480b0d79283bd57a011c788b8f0
sfmiller940 <sfm15@columbia.edu>
2015-09-07 17:48:23 -0700

    Update send_eth.py

--- pyethereum/send_eth.py
+++ pyethereum/send_eth.py
@@ -9,9 +9,21 @@ addr1 = utils.privtoaddr(key1)
 key2 = utils.sha3("37Dhsg17e92dfghKa Th!s i$ mUcH better r920167dfghjiHFGJsbcm")
 addr2 = utils.privtoaddr(key2)
 
+// Check balances
+s.block.get_balance(addr1)
+s.block.get_balance(addr2)
+
 // Set new address as coinbase and mine some coin.
 s.block.coinbase = addr1
 s.mine(n=10)
 
+// Check balances
+s.block.get_balance(addr1)
+s.block.get_balance(addr2)
+
 // Send 1000 wei from key1 to addr2.
 s.send(key1, addr2, 1000)
+
+// Check balances
+s.block.get_balance(addr1)
+s.block.get_balance(addr2)
