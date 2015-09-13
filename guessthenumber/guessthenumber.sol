7630ae733ef2d3a3b3c69c219d7fd39258b61964
sfmiller940 <sfm15@columbia.edu>
2015-09-12 19:30:25 -0700

    Create guessthenumber.sol

---
+++ guessthenumber/guessthenumber.sol
@@ -0,0 +1,34 @@
+contract guessthenumber{
+
+	address public owner;
+	uint public numGames;
+	mapping (uint => address) players;
+	mapping (uint => uint) numbers;
+	mapping (uint => uint) public ranges;
+	mapping (uint => uint) public wagers;
+
+	function guessthenumber(){
+		owner = msg.sender;
+	}
+
+	function newGame( uint number, uint range){
+		players[numGames] = msg.sender;
+		numbers[numGames] = number;
+		ranges[numGames] = range;
+		wagers[numGames] = msg.value;
+		numGames++;
+	}
+
+	function guessNumber(uint idx, uint number){
+		if(idx > numGames) return; 
+		if(number > ranges[idx]) return;
+		if(msg.value < wagers[idx] / ranges[idx]){
+			msg.sender.send( msg.value );
+			return;
+		}
+		if(msg.value > wagers[idx] / ranges[idx]) msg.sender.send( msg.value - ( wagers[idx] / ranges[idx] ) );
+		if(number == numbers[idx]){ msg.sender.send( wagers[idx] ); }
+		else{ players[idx].send( wagers[idx] / ranges[idx] ); }
+	}
+
+}
