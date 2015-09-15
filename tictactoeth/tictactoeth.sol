136a8c09fb097bcb7f492527ffa2d72bf7a1505d
sfmiller940 <sfm15@columbia.edu>
2015-09-14 22:34:32 -0700

    Update tictactoeth.sol

--- tictactoeth/tictactoeth.sol
+++ tictactoeth/tictactoeth.sol
@@ -24,23 +24,42 @@ contract tictactoeth{
 	mapping( uint => address ) public playerX;
 	mapping( uint => uint[9] ) public gameState;
 
+	event newGame( uint idx );
+	event newMove( uint idx );
+	event winGame( uint idx, address winner );
+	event expireGame( uint idx );
+	event staleGame( uint idx );
+
 	// Initialize new game.
 	function createGame(uint move, uint blockLife){
 		if( move > 8 || blockLife > 10000000000000 ){ 
+			msg.sender.send( msg.value );
 			return; 
 		}
 		deadline[numGames] = block.number + blockLife;
 		wager[numGames] = msg.value;
 		playerO[numGames] = msg.sender;
 		gameState[numGames][move] = 1;
+
+		newGame( numGames );
 		numGames++;
 	}
 
 	// Process new moves.
 	function createMove(uint idx, uint move){
 
+		uint bet = wager[idx] / 3;
+
+		// Check for expired.
+		if( deadline[idx] > block.number ){
+			msg.sender.send( msg.value );
+            playerO[idx].send( wager[idx] );
+            playerX[idx].send( bet );
+            expireGame(idx);
+		}
+
 		// Check for valid game and valid move.
-		if( idx >= numGames ||  move > 8 || gameState[idx][move] == 1 || gameState[idx][move] == 2 ){ 
+		if( idx >= numGames ||  move > 8 || gameState[idx][move] == 1 || gameState[idx][move] == 2 || wager[idx] == 0 ){ 
 			msg.sender.send( msg.value );
 			return;
 		}
@@ -57,17 +76,17 @@ contract tictactoeth{
 
 		// Process new player if second move.
 		if( 1 == numO + numX ){
-			if( msg.value < wager[idx] ){
+			if( msg.value < bet ){
 				msg.sender.send( msg.value );
 				return;
 			}
-			if( msg.value > wager[idx] ){
-				msg.sender.send( msg.value - wager[idx] );
+			if( msg.value > bet ){
+				msg.sender.send( msg.value - bet );
 			}
 			playerX[idx] = msg.sender;
 		}
 		// If not second move, return any ether to sender.
-		if ( 1 != numO + numX && msg.value > 0){
+		else if (  msg.value > 0 ){
 				msg.sender.send( msg.value );
 		}
 
@@ -79,41 +98,52 @@ contract tictactoeth{
 			gameState[idx][move] = 1;
 		}
 
-        // Check for win or stalemate.
-        if(numO + numX > 4){
-            if( (gameState[idx][0] == 1 && gameState[idx][3] == 1 && gameState[idx][6] == 1 ) ||
-                (gameState[idx][1] == 1 && gameState[idx][4] == 1 && gameState[idx][7] == 1 ) ||
-                (gameState[idx][2] == 1 && gameState[idx][5] == 1 && gameState[idx][8] == 1 ) ||
-                (gameState[idx][0] == 1 && gameState[idx][1] == 1 && gameState[idx][2] == 1 ) ||
-                (gameState[idx][3] == 1 && gameState[idx][4] == 1 && gameState[idx][5] == 1 ) ||
-                (gameState[idx][6] == 1 && gameState[idx][7] == 1 && gameState[idx][8] == 1 ) ||
-                (gameState[idx][0] == 1 && gameState[idx][4] == 1 && gameState[idx][8] == 1 ) ||
-                (gameState[idx][2] == 1 && gameState[idx][4] == 1 && gameState[idx][6] == 1 )
+        // Check for win
+        if(numO + numX > 3){
+            uint fee = ( wager[idx] + bet ) / 100;
+            if( ((gameState[idx][0] == 1) && (gameState[idx][3] == 1) && (gameState[idx][6] == 1) ) ||
+                ((gameState[idx][1] == 1) && (gameState[idx][4] == 1) && (gameState[idx][7] == 1) ) ||
+                ((gameState[idx][2] == 1) && (gameState[idx][5] == 1) && (gameState[idx][8] == 1) ) ||
+                ((gameState[idx][0] == 1) && (gameState[idx][1] == 1) && (gameState[idx][2] == 1) ) ||
+                ((gameState[idx][3] == 1) && (gameState[idx][4] == 1) && (gameState[idx][5] == 1) ) ||
+                ((gameState[idx][6] == 1) && (gameState[idx][7] == 1) && (gameState[idx][8] == 1) ) ||
+                ((gameState[idx][0] == 1) && (gameState[idx][4] == 1) && (gameState[idx][8] == 1) ) ||
+                ((gameState[idx][2] == 1) && (gameState[idx][4] == 1) && (gameState[idx][6] == 1) )
                 ){
-                    playerO[idx].send( wager[idx] * 2 );
+                    playerO[idx].send( wager[idx] + bet - fee);
+                    wager[idx] = 0;
+                    winGame(idx, playerO[idx]);
+                    collectedFees += fee;
                     return;
                 }
-            if( (gameState[idx][0] == 2 && gameState[idx][3] == 2 && gameState[idx][6] == 2 ) ||
-                (gameState[idx][1] == 2 && gameState[idx][4] == 2 && gameState[idx][7] == 2 ) ||
-                (gameState[idx][2] == 2 && gameState[idx][5] == 2 && gameState[idx][8] == 2 ) ||
-                (gameState[idx][0] == 2 && gameState[idx][1] == 2 && gameState[idx][2] == 2 ) ||
-                (gameState[idx][3] == 2 && gameState[idx][4] == 2 && gameState[idx][5] == 2 ) ||
-                (gameState[idx][6] == 2 && gameState[idx][7] == 2 && gameState[idx][8] == 2 ) ||
-                (gameState[idx][0] == 2 && gameState[idx][4] == 2 && gameState[idx][8] == 2 ) ||
-                (gameState[idx][2] == 2 && gameState[idx][4] == 2 && gameState[idx][6] == 2 )
+            else if( ((gameState[idx][0] == 2) && (gameState[idx][3] == 2) && (gameState[idx][6] == 2) ) ||
+                ((gameState[idx][1] == 2) && (gameState[idx][4] == 2) && (gameState[idx][7] == 2) ) ||
+                ((gameState[idx][2] == 2) && (gameState[idx][5] == 2) && (gameState[idx][8] == 2) ) ||
+                ((gameState[idx][0] == 2) && (gameState[idx][1] == 2) && (gameState[idx][2] == 2) ) ||
+                ((gameState[idx][3] == 2) && (gameState[idx][4] == 2) && (gameState[idx][5] == 2) ) ||
+                ((gameState[idx][6] == 2) && (gameState[idx][7] == 2) && (gameState[idx][8] == 2) ) ||
+                ((gameState[idx][0] == 2) && (gameState[idx][4] == 2) && (gameState[idx][8] == 2) ) ||
+                ((gameState[idx][2] == 2) && (gameState[idx][4] == 2) && (gameState[idx][6] == 2) )
                 ){
-                    playerX[idx].send( wager[idx] * 2 );
+                    playerX[idx].send( wager[idx] + bet - fee);
+                    wager[idx] = 0;
+                    winGame(idx, playerX[idx]);
+                    collectedFees += fee;
                     return;
                 }
+	        // Check for stalemate.
+	        else if ( numO + numX == 9 ){
+	            playerO[idx].send( wager[idx] );
+	            playerX[idx].send( bet );
+	            wager[idx] = 0;
+	            staleGame(idx);
+	            return;
+
+	        }
+
         }
 
-        // Check for stalemate or expired.
-        if ( numO + numX == 9 || deadline[idx] < block.number ){
-            uint fee = wager[idx] / 100;
-            collectedFees += 2 * fee;
-            playerO[idx].send( wager[idx] - fee );
-            playerX[idx].send( wager[idx] - fee );
-        }
+        newMove(idx);
 
 	}
 
